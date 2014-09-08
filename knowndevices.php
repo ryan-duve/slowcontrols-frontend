3965797ffb084ecbbd0de1b8cab2ab1364c376ec
Ryan Duve <duve@virginia.edu>
2014-09-08 11:32:11 -0400

    debug change

--- knowndevices.php
+++ knowndevices.php
@@ -37,7 +37,7 @@ $KNOWN_DEVICES=array(
   ),
   "d1"=>array(
     "displayName"=>"dee un",
-    "units"=>"arbinitos",
+    "units"=>"arbis",
     "table"=>"slowcontrolreadings",
     "color"=>"green",
   ),
