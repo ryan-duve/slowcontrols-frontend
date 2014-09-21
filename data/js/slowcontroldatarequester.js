1f8040032a2a884cf28a852223666be6caa2c0e8
Ryan Duve <ryan.duve@gmail.com>
2014-09-20 20:48:47 -0400

    use moment.js for timestamp control/manipulation; get bootstrap-esque input fields working; provide submit functionality - THE FORM NOW RECEIVES A VALID, TIME-APPRORIATE report REPONSE FROM THE SERVER

--- data/js/slowcontroldatarequester.js
+++ data/js/slowcontroldatarequester.js
@@ -13,20 +13,60 @@ $(function (){
   });
 
   //bind form submission
-  $("#submit").on('click',function(){
-    console.log('submitting form');
+  $("#submit").on('click',function(event){
+    event.preventDefault();
+    submitDataRequest();
   });
 
 });
 
+//on click submit button
+function submitDataRequest(){
+  //get timestamps
+  begTimestamp=moment($('#begTime').val());
+  endTimestamp=moment($('#endTime').val());
+
+  //nData is difference in minutes
+  differenceInSeconds=endTimestamp.diff(begTimestamp,'seconds');
+  nData=Math.ceil(differenceInSeconds/60); //60 seconds/min
+
+  console.log('nData='+nData);
+
+  //get device
+  dev=$("input:radio[name=devNameRadioButton]:checked").val();
+
+  //format endTimestamp
+  endTimestamp=$('#endTime').val();
+  console.log('sending endTimestamp='+endTimestamp);
+
+  $.ajax({
+    url:"../getReport.php",
+    type:"POST",
+    data:getReportParams(nData,dev,endTimestamp),
+    dataType:"json",
+    success: onReportReceived
+  }); 
+
+}
+
+function onReportReceived(report){
+  console.log("report incoming:");
+  console.log(report);
+}
+
+function getReportParams(nData,dev,endTimestamp){
+  var reportparams={};
+  reportparams['incomingDevs']=[];
+  reportparams['incomingDevs'].push(dev);
+  reportparams['nData']=nData;
+  reportparams['endTimestamp']=endTimestamp;
+  return reportparams;
+}
+
 function populateTimeFields(){
   //get new date
-  currentTimestamp= new Date();
-  pastTimestamp = new Date(currentTimestamp);
-  pastTimestamp.setMinutes(currentTimestamp.getMinutes()-1);
-
-  currentTimestamp=currentTimestamp.toMysqlFormat();
-  pastTimestamp=pastTimestamp.toMysqlFormat();
+  currentTimestamp=moment().format('YYYY-MM-DD HH:mm:ss');
+  pastTimestamp = moment().subtract(1,'minutes').format('YYYY-MM-DD HH:mm:ss');
 
   //set fields
   $('#begTime').val(pastTimestamp);
@@ -34,24 +74,10 @@ function populateTimeFields(){
 
 }
 
-Date.prototype.toMysqlFormat = function() {
-  return this.getUTCFullYear() + "-" + twoDigits(1 + this.getUTCMonth()) + "-" + twoDigits(this.getUTCDate()) + " " + twoDigits(this.getUTCHours()) + ":" + twoDigits(this.getUTCMinutes()) + ":" + twoDigits(this.getUTCSeconds());
-};
-
-function twoDigits(d) {
-  if(0 <= d && d < 10) return "0" + d.toString();
-  if(-10 < d && d < 0) return "-0" + (-1*d).toString();
-  return d.toString();
-}
-
-function fillTimestampFieldsWithCurrentTime(){
-  //get timestamp in 'YYYY-MM-DD HH:MM:SS' format
-
-}
-
 function onDeviceNamesReceived(devNames){
-  console.log(devNames);
   makeRadioListFromDevNames(devNames);
+
+  $("input:radio[name=devNameRadioButton]:first").attr('checked', true);
 }
 
 //makes radio list of devices
@@ -59,14 +85,14 @@ function makeRadioListFromDevNames(devNames){
   devNames.forEach(function(dev){
     //the label
     $('<label />',{
-      text:dev,
+    text:dev,
     id:'devNameLabel-'+dev,
     class:'radio',
     }).appendTo('#devNameSelectionWrapper');
 
     $('<input>',{
       type:'radio',
-      value:'devNameRadioButton-'+dev,
+      value:dev,
       name:'devNameRadioButton',
     }).appendTo('#devNameLabel-'+dev);
   });
