6c31d2e961e62ae2725b8c1179aa28e97eb8ad6a
Ryan Duve <duve@virginia.edu>
2014-10-24 00:34:52 -0400

    superifical.changes

--- data/js/slowcontroldatarequester.js
+++ data/js/slowcontroldatarequester.js
@@ -14,6 +14,7 @@ $(function (){
   //bind form submission
   $("#submit").on('click',function(event){
     event.preventDefault();
+    $('.graph-container').show();
     submitDataRequest();
   });
 
